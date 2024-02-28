45ebb5595b13c0152913a7d41431f57af599527c
Shwetank Kumar <shwetank.kumar@gmail.com>
2024-02-28 23:27:54 +0000

    adding shakespeare-1 to debug training

--- models.py
+++ models.py
@@ -69,18 +69,22 @@ class BlockScratch(nn.Module):
     
     
 class Xformer_Scratch(nn.Module):
-    def __init__(self, emb_dim, vocab_size, num_heads, block_length, dropout):
+    def __init__(self, emb_dim, vocab_size, num_heads, num_layers, block_length, dropout):
         super().__init__()
         self.token_embedding = nn.Embedding(vocab_size + 1, emb_dim)
         self.pos_embedding = nn.Embedding(vocab_size + 1, emb_dim)
         # self.sa_head = MultiHeadAttention(emb_dim//4, 4, emb_dim, block_length)
         # self.ff = Feedforward(emb_dim)
-        self.blocks = nn.Sequential(
-            BlockScratch(emb_dim, num_heads, block_length, dropout), 
-            BlockScratch(emb_dim, num_heads, block_length, dropout),
-            BlockScratch(emb_dim, num_heads, block_length, dropout),
-            nn.LayerNorm(emb_dim)
-        )
+        # self.blocks = nn.Sequential(
+        #     BlockScratch(emb_dim, num_heads, block_length, dropout), 
+        #     BlockScratch(emb_dim, num_heads, block_length, dropout),
+        #     BlockScratch(emb_dim, num_heads, block_length, dropout),
+        #     nn.LayerNorm(emb_dim)
+        # )
+        blocks = [BlockScratch(emb_dim, num_heads, block_length, dropout) for _ in range(num_layers)]
+        blocks.append(nn.LayerNorm(emb_dim))
+        
+        self.blocks = nn.Sequential(*blocks)
         self.lm_head = nn.Linear(emb_dim, vocab_size)
 
     def forward(self, x, targets=None):
