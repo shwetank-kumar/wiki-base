ae014db6438ad1d5219e365a8ae8921c53c4c095
Shwetank Kumar <shwetank.kumar@gmail.com>
2024-03-06 21:39:49 -0800

    shakespeare with right dataloader

--- utils.py
+++ utils.py
@@ -14,49 +14,42 @@ def create_dataset(input_path):
     max_length = max(len(w) for w in words)
     return words, vocab, max_length
 
-class CharDataset(Dataset):
-    def __init__(self, words, vocab, max_length) -> None:
-        
-        self.words = words
-        self.vocab = vocab
-        self.max_length = max_length
-        self.stoi = {s: i+1 for i,s in enumerate(vocab)}
-        self.itos = {s: i for i,s in self.stoi.items()}
+def get_batch(dataset, device, block_size=256, batch_size=64):
+    idx = torch.randint(len(dataset) - block_size, (batch_size, ))
+    x = torch.stack([dataset[i:i+block_size] for i in idx])
+    y = torch.stack([dataset[i+1:i+block_size+1] for i in idx])
 
-    def __len__(self):
-        return len(self.words)
+    return x.to(device), y.to(device)
 
-    def encode(self, words): 
-        print(words)
-        return torch.tensor([self.stoi[w] for w in words], dtype = torch.long)
-    
-    def decode(self, tokens): 
-        return ''.join(self.itos[t] for t in tokens)
-    
-    ## This function makes sense for a series of names. Requires an update for chunks inside a piece of text
-    # def __getitem__(self, idx):
-    #     word = self.words[idx]
-    #     x = torch.zeros(self.max_length+1, dtype=torch.long)
-    #     y = torch.zeros(self.max_length+1, dtype=torch.long)
-    #     ix = self.encode(word)
-    #     x[1:1+len(ix)] = ix
-    #     y[:len(ix)] = ix
-    #     y[len(ix)+1:] = -1
-    #     return x, y
+@torch.no_grad()
+def estimate_loss(model, block_size, batch_size, train_data, val_data, device, eval_iters):
+    data = {'train': train_data, 'val': val_data}
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(data[split], device, block_size, batch_size)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
 
 
 @torch.inference_mode()
-def evaluate_loss(model, tr_loader, te_loader, device, num_batches = 10):
+def evaluate_loss_char_ds(model, tr_ds, te_ds, num_batches = 10):
     model.eval()
     loss_tr = []
     loss_te = []
     for n in range(num_batches):
-        Xtr, Ytr = next(iter(tr_loader))
-        Xte, Yte = next(iter(te_loader))
-        Xtr = Xtr.to(device)
-        Ytr = Ytr.to(device)
-        Xte = Xte.to(device)
-        Yte = Yte.to(device)
+        Xtr, Ytr = next(iter(tr_ds))
+        Xte, Yte = next(iter(te_ds))
+        # Xtr = Xtr.to(device)
+        # Ytr = Ytr.to(device)
+        # Xte = Xte.to(device)
+        # Yte = Yte.to(device)
+        # print(Xtr.shape, Ytr.shape, Xte.shape, Yte.shape)
         _, train_loss = model(Xtr, Ytr)
         _, test_loss = model(Xte, Yte)
         loss_tr.append(train_loss)
@@ -66,6 +59,7 @@ def evaluate_loss(model, tr_loader, te_loader, device, num_batches = 10):
     mean_test_loss = torch.tensor(loss_te).mean().item()
     model.train()
     return(mean_train_loss, mean_test_loss)
+
 
 @torch.no_grad()
 def _generate(model, idx, max_new_tokens, device, block_size=16):
@@ -97,7 +91,7 @@ def print_samples(model, train_data, max_new_tokens, device, num=10):
         row = row[:crop_index]
         print(train_data.decode(row))
 
-def get_lr_loss(model, optimizer, train_dataloader, num_epochs, device, lr_start_exp=-3, lr_end_exp=0.5):
+def get_lr_loss(model, optimizer, dataset, num_epochs, device, block_size, batch_size, lr_start_exp=-3, lr_end_exp=0.5):
 
     lrexp = torch.linspace(lr_start_exp, lr_end_exp, num_epochs, requires_grad=False)
     lrs_val = 10**lrexp
@@ -111,9 +105,8 @@ def get_lr_loss(model, optimizer, train_dataloader, num_epochs, device, lr_start
         for g in optimizer.param_groups:
             g['lr'] = lrs_val[epoch]
 
-        xb, yb = next(iter(train_dataloader))
-        xb = xb.to(device)
-        yb = yb.to(device)
+        xb, yb = get_batch(dataset, device, block_size, batch_size)
+        # xb, yb = next(iter(train_ds))
         # print(xb.shape, yb.shape)
 
         # ix = torch.randint(0, xb.shape[0], (batch_size,))
